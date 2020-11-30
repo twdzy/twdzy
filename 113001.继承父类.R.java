9e170ba1a8ef9cfcf47140db366e79893727ea85
twdzy <74773456+twdzy@users.noreply.github.com>
2020-11-30 15:30:23 +0800

    Update 113001.继承父类.R.java

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -3,5 +3,5 @@ package pac;
 public class R {
 	static String boy_filePath="img/boy.gif";
 	static String rirl_filePath="img/girl.gif";
-	static int[][]array2d={{1,2,3},{1,2,3}};
+	static int[][]array2d={{1,2,3},{4,5,6}};
 }
