7bedb72ad39f9951af610106bf83fcecd1da0200
twdzy <74773456+twdzy@users.noreply.github.com>
2020-12-07 15:33:36 +0800

    Update 120701.寻找0的相邻坐标.Find.java

--- 120701.寻找0的相邻坐标.Find.java
+++ 120701.寻找0的相邻坐标.Find.java
@@ -1,14 +1,46 @@
 package pac;
-
 public class Find {
-	void finalizeor(int array2d[][]){
+	void finalizero(int array2d[][]){
 		for (int row = 0; row < array2d.length; row++) {
 			for (int column = 0; column < array2d[0].length; column++) {
 				if (array2d[row][column]==0) {
 					System.out.println("R_zero("+column+","+row+")");
+					
+				}
+			}
+		}
+	}
+	void R_left(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				if (array2d[row][column]==0) {
 					System.out.println("R_left("+(column-1)+","+row+")");
+				}
+			}
+		}
+	}
+	void R_righ(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				if (array2d[row][column]==0) {
 					System.out.println("R_right("+(column+1)+","+row+")");
+				}
+			}
+		}
+	}
+	void R_up(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				if (array2d[row][column]==0) {
 					System.out.println("R_up("+column+","+(row-1)+")");
+				}
+			}
+		}
+	}
+	void R_down(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				if (array2d[row][column]==0) {
 					System.out.println("R_down("+column+","+(row+1)+")");
 				}
 			}
