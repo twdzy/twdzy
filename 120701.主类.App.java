84069a9bddf5112a02c3bc888a05ec12d51a4143
twdzy <74773456+twdzy@users.noreply.github.com>
2020-12-07 15:27:21 +0800

    Create 120701.主类.App.java

---
+++ 120701.主类.App.java
@@ -0,0 +1,15 @@
+package pac;
+
+public class App {
+public static void main(String[] args) {
+	int array2d[ ][ ]={
+			{1,2,3,4,5,6},
+			{7,8,9,10,0,12},
+			{8,8,9,10,11,12},
+			{7,8,3,10,11,12},
+			{88,66,92,102,151,999}
+			};
+	new Find().finalizeor(array2d);
+	}
+}
+
