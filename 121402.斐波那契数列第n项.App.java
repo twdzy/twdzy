3cccfca0955097885d0d2c2a4f0ef6d353d3bbe9
twdzy <74773456+twdzy@users.noreply.github.com>
2020-12-14 15:14:34 +0800

    Create 121402.斐波那契数列第n项.App.java

---
+++ 121402.斐波那契数列第n项.App.java
@@ -0,0 +1,25 @@
+package pac;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		
+		int n,fn;
+		System.out.println("输入一个整数n，取值区间：(1<=n<=40)。");
+		java.util.Scanner s = new Scanner(System.in);
+			n=s.nextInt();
+			fn=function(n);
+			if(n>=1 && n<=40){
+				System.out.println("斐波那契数列第"+n+"项为："+fn);
+			}else {
+				System.out.println("超出范围");
+			}
+			
+		}
+		public static int function(int n){
+		if(n==1 || n==2 && n>=1 && n<=40) return 1;
+		return function(n-1)+function(n-2);
+		}
+		
+}
