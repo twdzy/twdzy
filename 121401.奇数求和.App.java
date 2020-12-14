56030ad85558217df28b7cb5e0c194fd61c27f5a
twdzy <74773456+twdzy@users.noreply.github.com>
2020-12-14 14:53:56 +0800

    Create 121401.奇数求和.App.java

---
+++ 121401.奇数求和.App.java
@@ -0,0 +1,11 @@
+package pac;
+
+public class App {
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i <= 100; i += 2) {
+		sum += i;
+		}
+		System.out.println("1到100的奇数和为：" + sum);
+	}
+}
